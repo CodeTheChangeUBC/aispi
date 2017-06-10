3e3c2d7dc7f27651878b2ee8ca2f607d47f8c73f
TuanPham303 <phamcongtuan303@gmail.com>
2017-06-10 15:31:36 -0700

    add timeline

--- design/home.js
+++ design/home.js
@@ -1,17 +1,18 @@
 const MONTHS = ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEPT','OCT','NOV','DEC']
 
 var Calendar = new Vue({
-  el: '#calendar__wrap',
+  el: '.home__inline',
   data: {
     current: null,
     month: '',
     year: 0,
     days: [],
     eventMap: {},
-    times: [
-    '6:00 am','7:00 am','8:00 am','9:00 am','10:00 am','11:00 am','12:00 on',
-    '1:00 pm','2:00 pm','3:00 pm','4:00 pm','5:00 pm','6:00 pm','7:00 pm','8:00 pm', '9:00 pm'
-    ]
+  },
+  methods: {
+    toggleTimeLine: function(){
+      calendar.inView = !calendar.inView
+    }
   }
 })
 
