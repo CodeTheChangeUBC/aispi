8bf7ad13c9e23718996c71f7e3ccbbc51e98e16a
TuanPham303 <phamcongtuan303@gmail.com>
2017-07-11 18:52:49 -0700

    add booked event

--- design/timeline.js
+++ design/timeline.js
@@ -1,16 +1,18 @@
 var calendar=new Vue({
    el: '.viewPopup',
    data: {
-      inView: true,
+      inView: false,
       times: [
        '6:00 am','7:00 am','8:00 am','9:00 am','10:00 am','11:00 am','12:00 am',
        '1:00 pm','2:00 pm','3:00 pm','4:00 pm','5:00 pm','6:00 pm','7:00 pm','8:00 pm', '9:00 pm'
       ],
-      bookedEvent: {
-        name: 'asasd',
-        startTime: '6:00 am',
-        length: '120'
-      }
+      bookedEvent : [
+      {
+        name: 'Vancouver School',
+        startTime: '9:00 am',
+        length: '360'
+      },
+      ],
    },
    methods: {
     toggleTimeLine: function(e){
@@ -18,10 +20,13 @@ var calendar=new Vue({
         return
       }
       calendar.inView = !calendar.inView  
-    },  
+    },   
   }
 })
-var height = calendar.bookedEvent.length/60*20;
-var start = '10vh'+ calendar.times.indexOf(calendar.bookedEvent.startTime)*20
+/*var height = bookedEvent.length/60*20;
+var start = '10vh'+ times.indexOf(bookedEvent.startTime)*20
 console.log(height)
 console.log(start)
+*/
+var marginTop = calendar.times.indexOf(calendar.bookedEvent[0].startTime)*20;
+console.log(marginTop);
